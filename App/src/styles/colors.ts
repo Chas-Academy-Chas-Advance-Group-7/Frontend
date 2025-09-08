924e65a7d6031fb22e3d24136a6a387003556548
tovaalicia <tovahansens@gmail.com>
2025-09-08 10:23:05 +0200

    colors in colors.ts

---
+++ App/src/styles/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  // Grundfärger  
+  primary: '#08519C',
+  secondary: '#191919',
+  base: '#FFFFFF',
+
+
+  // Layout och bakgrunder
+  background: '#cefabf',
+  card: '#84ca6f',
+  headerBackground: ' #FCFFF5',
+  tabBackground: '#66c181',
+
+
+  //Knapp icon
+  buttonColorGrey: ' #8C8C8C',
+  buttonColorGradiet: 'linear-gradient(90deg, #6BAED6 0%, #08519C 100%)',
+};
