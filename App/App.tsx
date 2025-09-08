db21b1438f4bc6ccc8aff82aa77be8ed70da2df7
tovaalicia <tovahansens@gmail.com>
2025-09-08 12:25:40 +0200

    Header

--- App/App.tsx
+++ App/App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import GradientButton from "./src/components/GradientButton";
+import { Header } from "react-native/Libraries/NewAppScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <GradientButton />
+      <Header />
     </View>
   );
 }
