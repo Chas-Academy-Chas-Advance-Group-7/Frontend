6cbdeb2d435dc1f5d8fc5e57cf311776b07edaa6
tovaalicia <tovahansens@gmail.com>
2025-09-08 12:25:27 +0200

    merge

--- App/App.tsx
+++ App/App.tsx
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import Header from './src/components/Header';
-=======
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import GradientButton from "./src/components/GradientButton";
->>>>>>> 38dee3f4b0c3c34faf8bc3994fbf0d624d095424
 
 export default function App() {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
-      <Header />
-      <StatusBar style="auto" />
-=======
       <GradientButton />
->>>>>>> 38dee3f4b0c3c34faf8bc3994fbf0d624d095424
     </View>
   );
 }
