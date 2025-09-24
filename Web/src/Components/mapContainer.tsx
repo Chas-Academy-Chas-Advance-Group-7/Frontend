e73a941538dbb4f228385bd5bcba861f7ca2c5cd
Hannakind <Hanna.kindholm@chasacademy.se>
2025-09-24 11:14:12 +0200

    new json

--- Web/src/Components/mapContainer.tsx
+++ Web/src/Components/mapContainer.tsx
@@ -2,13 +2,10 @@ import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-
-// Default icon fix (annars syns inte Leaflet-markers i CRA/Vite)
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
-// sätt standardikon för Leaflet
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: markerIcon2x,
