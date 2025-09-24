7b3eafd98d5fb2fae1d7602b947124ad8ec13108
sandrakarlberg <sandra.karlberg@hotmail.com>
2025-09-24 16:38:39 +0200

    Added header to UserScreen

--- App/src/screens/UserScreen.tsx
+++ App/src/screens/UserScreen.tsx
@@ -1,14 +1,17 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Header from "../components/Header";
 
 const UserScreen = () => {
   return (
-    <View>
-      <Text>UserScreen</Text>
+    <View style={styles.container}>
+      <Header />
     </View>
   );
 };
 
 export default UserScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", padding: 40 },
+});
