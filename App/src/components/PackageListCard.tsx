547564725e8741f528d2ac09a5ab987fdb7f95fc
sandrakarlberg <sandra.karlberg@hotmail.com>
2025-10-07 18:13:56 +0200

    Modal moved into separate component

--- App/src/components/PackageListCard.tsx
+++ App/src/components/PackageListCard.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View, Modal, Pressable, Image } from "react-native";
-import React, { useState } from "react";
+import { useState } from "react";
 import { colors } from "../styles/colors";
-import GradientButton from "./GradientButton";
+import PackageModal from "./PackageModal";
 
 const PackageListCard = ({
   packageItem,
@@ -18,36 +18,11 @@ const PackageListCard = ({
   return (
     <Pressable style={styles.listItem} onPress={() => setModalVisible(true)}>
       <Text style={styles.listItemText}>{packageItem.package_name}</Text>
-      <Modal transparent visible={modalVisible} animationType="fade">
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            <View style={styles.modalInfo}>
-              <Image
-                source={require("../assets/package_icon.png")}
-                style={styles.modalImage}
-              />
-              <Text style={styles.modalTitle}>{packageItem.package_name}</Text>
-              <Text style={styles.modalText}>
-                Temperatur: {packageItem.package_temp}°C
-              </Text>
-              <Text style={styles.modalText}>
-                Fuktighet: {packageItem.package_humidity}%
-              </Text>
-            </View>
-            <GradientButton
-              colors={[colors.buttonGradientLeft, colors.buttonGradientRight]}
-              title="Markera som levererad"
-              onPress={() => setModalVisible(false)}
-            />
-            <GradientButton
-              colors={[colors.buttonColorGrey, colors.buttonColorGrey]}
-              title="Gå tillbaka"
-              onPress={() => setModalVisible(false)}
-              style={{ marginTop: 10 }}
-            />
-          </View>
-        </View>
-      </Modal>
+      <PackageModal
+      packageItem={packageItem}
+      modalVisible={modalVisible}
+      setModalVisible={setModalVisible}
+      />
     </Pressable>
   );
 };
@@ -65,47 +40,5 @@ const styles = StyleSheet.create({
   listItemText: {
     fontFamily: "InterRegular",
     fontSize: 16,
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0,0,0,0.8)",
-  },
-  modalContent: {
-    backgroundColor: "white",
-    borderRadius: 10,
-    padding: 20,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  modalInfo: {
-    padding: 10,
-    marginBottom: 10,
-  },
-  modalImage: {
-    height: 35,
-    width: 35,
-    alignSelf: "center",
-    marginVertical: 15,
-  },
-  modalTitle: {
-    fontFamily: "InterBold",
-    fontSize: 20,
-    marginBottom: 20,
-    marginTop: 20,
-    color: colors.textPrimary,
-    textAlign: "center",
-  },
-  modalText: {
-    fontFamily: "InterRegular",
-    fontSize: 16,
-    marginBottom: 10,
-    color: colors.textPrimary,
-    textAlign: "center",
   },
 });
